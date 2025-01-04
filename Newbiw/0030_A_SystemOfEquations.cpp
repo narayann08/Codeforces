04fe5ca130696abac64a685e6e8716181ba65dae
Naransing Darbar <166582433+narayann08@users.noreply.github.com>
2025-01-04 22:58:07 +0530

    Add files via upload

---
+++ Newbiw/0030_A_SystemOfEquations.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define int long long
+
+signed main(){
+    int n, m, ans=0;
+    cin >> n >> m;
+    for(int a=0; a*a<=n; a++){
+        for(int b=0; b*b<=m; b++){
+            if(((a*a + b) == n) && ((b*b + a) == m)) {
+                ans++;
+            }
+        }
+    }
+    cout << ans;    
+}
