cd3f3bd563a4b338cdc420826a91ef8007a985ed
Naransing Darbar <166582433+narayann08@users.noreply.github.com>
2025-01-08 21:52:32 +0530

    Add files via upload

---
+++ Newbiw/0031_A_MagicNumber.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define int long long
+
+signed main(){
+    string n; cin >> n;
+    int done=1, i=0;
+    while(i<n.size()){
+        if(i+2<n.size() && n.substr(i,3)=="144"){
+            i += 3;
+        }
+        else if(i+1<n.size() && n.substr(i,2)=="14"){
+            i += 2;
+        }
+        else if(n[i]=='1'){
+            i++;
+        }
+        else{
+            done=0;
+            break;
+        }
+    }    
+    if(done){
+        cout << "YES";
+    }
+    else{
+        cout << "NO";
+    }
+}
